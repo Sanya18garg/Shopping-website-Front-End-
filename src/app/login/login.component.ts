a50ba4f28fba34e4366d4caeeb94123f983d8112
sanya18garg <1976rajnigarg@gmail.com>
2019-10-18 21:57:11 +0530

    7 commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,7 +26,7 @@ login()
 {
   console.log(this.username);
   console.log(this.password);
-  this.authservice.authenticate(this.username,this.password).subscribe(data=>{this.service.isloggedin(true);this.router.navigate(['home']);});
+  this.authservice.authenticate(this.username,this.password).subscribe(data=>{this.service.isloggedin(true);this.router.navigate(['products']);});
 
 
 }
